2d89209bfcaf5a2c12a003557aa03eb9c98d156c
Fryy55 <ivan_ugu55@icloud.com>
2025-04-21 22:43:36 +0500

    v1.0.0 Changelog: 	- Initial release

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,52 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
 
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+class $modify(HPauseLayer, PauseLayer) {
+	$override void customSetup() {
+		PauseLayer::customSetup();
 
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+		auto sprite = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
+		auto exMark = CCSprite::createWithSpriteFrameName("exMark_001.png");
+		exMark -> setScale(0.6f);
+		exMark -> setPosition({7.f, 10.f});
+		exMark -> setID("ex-mark-sprite"_spr);
+		sprite -> setScale(0.6f);
+		sprite -> setID("game-quit-button-sprite"_spr);
+
+		sprite -> addChild(exMark);
+
+		auto gameQuitButton = CCMenuItemSpriteExtra::create(
+			sprite,
 			this,
-			menu_selector(MyMenuLayer::onMyButton)
+			menu_selector(HPauseLayer::onGameQuit)
 		);
+		gameQuitButton -> setID("game-quit-button"_spr);
 
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+		auto menu = this -> getChildByID("right-button-menu");
+		menu -> addChild(gameQuitButton);
+		menu -> updateLayout();
 
-		myButton->setID("my-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
+		return;
 	}
 
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+	void onGameQuit(CCObject* sender) {
+		if (Mod::get() -> getSettingValue<bool>("confirm") == true)
+			createQuickPopup(
+				"Quit Game",
+				"Are you sure you want to <cr>quit</c>?",
+				"Cancel", "Yes",
+				300.f,
+				[](auto, bool yes) {
+					if (yes)
+						utils::game::exit();
+				}
+			);
+		else
+			utils::game::exit();
+
+		return;
 	}
 };
